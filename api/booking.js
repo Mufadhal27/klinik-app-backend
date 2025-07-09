e1fc93fceb74101bfda5457dc642dbe541d1da97
nunut851 <nunut3371@gmail.com>
2025-07-09 21:58:14 +0700

    new update

--- api/booking.js
+++ api/booking.js
@@ -2,6 +2,14 @@ const dbConnect = require("../utils/dbConnect");
 const Booking = require("../models/Booking");
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     await dbConnect();
   } catch (error) {
